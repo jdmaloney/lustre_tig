502c6a2fe5a68a5d907fe7d9717420c7b514c21b
malone12 <malone12@illinois.edu>
2021-10-28 00:18:06 -0500

    Lustre Quota start with id_dump code

---
+++ lustre_quota_id_dump.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /etc/telegraf/lustre/lustre_config
+
+## Node roll; how does it need to participate
+if [ -z "$(cat /proc/mounts | grep "MDT")" ]; then
+	## No MDTs, just exit
+	exit 0
+else
+	## I have MDT(s), dumping its idse
+	MDTs=($(ls /proc/fs/lustre/osd-ldiskfs/ | grep MDT | xargs))
+        for m in ${MDTs[@]}
+        do
+                lctl get_param osd-ldiskfs.${m}.quota_slave_dt.acct_user | grep id | awk '{print $3}' > ${admin_dir}/${m}_user_quota
+                lctl get_param osd-ldiskfs.${m}.quota_slave_dt.acct_group | grep id | awk '{print $3}' > ${admin_dir}/${m}_group_quota
+                lctl get_param osd-ldiskfs.${m}.quota_slave_dt.acct_project | grep id | awk '{print $3}' > ${admin_dir}/${m}_project_quota
+        done
+fi

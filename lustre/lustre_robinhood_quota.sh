4ef6b9cff4c2cc03ff298d8ae9205d47367d732e
malone12 <malone12@illinois.edu>
2023-03-30 20:12:07 -0500

    Added breakout for inode types per project/user/group

--- lustre/lustre_robinhood_quota.sh
+++ lustre/lustre_robinhood_quota.sh
@@ -15,6 +15,8 @@ do
 		IFS=" " read -r user_name bytes_used inodes <<< $(echo ${line})
 		IFS=" " read -r sz0 sz1 sz32 sz1K sz32K sz1M sz32M sz1G sz32G sz1T <<< $(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select sum(sz0),sum(sz1),sum(sz32),sum(sz1K),sum(sz32K),sum(sz1M),sum(sz32M),sum(sz1G),sum(sz32G),sum(sz1T) from ACCT_STAT where uid = '"${user_name}"' and projid = '"${p}"' and type = 'file';" | tail -n +2 | sed 's/\t/\ /g' | sed 's/NULL/0/g')
 		echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},user_name=${user_name} bytes_used=${bytes_used},inodes=${inodes},sz0=${sz0},sz1=${sz1},sz32=${sz32},sz1K=${sz1K},sz32K=${sz32K},sz1M=${sz1M},sz32M=${sz32M},sz1G=${sz1G},sz32G=${sz32G},sz1T=${sz1T}"
+        type_values=$(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select type,sum(count) from ACCT_STAT where uid = '"${user_name}"' and projid = '"${p}"' group by type;" | tail -n +2 | sed 's/\t/=/' | xargs | sed 's/\ /,/g')
+        echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},user_name=${user_name},report=inode_type ${type_values}"
 	done <"${tfile}"
 
 	## Gather per group stats in each project
@@ -23,11 +25,16 @@ do
                 IFS=" " read -r group_name bytes_used inodes <<< $(echo ${line})
                 IFS=" " read -r sz0 sz1 sz32 sz1K sz32K sz1M sz32M sz1G sz32G sz1T <<< $(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select sum(sz0),sum(sz1),sum(sz32),sum(sz1K),sum(sz32K),sum(sz1M),sum(sz32M),sum(sz1G),sum(sz32G),sum(sz1T) from ACCT_STAT where gid = '"${group_name}"' and projid = '"${p}"' and type = 'file';" | tail -n +2 | sed 's/\t/\ /g' | sed 's/NULL/0/g')
                 echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},group_name=${group_name} bytes_used=${bytes_used},inodes=${inodes},sz0=${sz0},sz1=${sz1},sz32=${sz32},sz1K=${sz1K},sz32K=${sz32K},sz1M=${sz1M},sz32M=${sz32M},sz1G=${sz1G},sz32G=${sz32G},sz1T=${sz1T}"
+        type_values=$(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select type,sum(count) from ACCT_STAT where gid = '"${group_name}"' and projid = '"${p}"' group by type;" | tail -n +2 | sed 's/\t/=/' | xargs | sed 's/\ /,/g')
+        echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},group_name=${group_name},report=inode_type ${type_values}"
         done <"${tfile}"
 
 	## Gather Aggregate Project Histogram
 	IFS=" " read -r sz0 sz1 sz32 sz1K sz32K sz1M sz32M sz1G sz32G sz1T <<< $(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select sum(sz0),sum(sz1),sum(sz32),sum(sz1K),sum(sz32K),sum(sz1M),sum(sz32M),sum(sz1G),sum(sz32G),sum(sz1T) from ACCT_STAT where projid = '"${p}"' and type = 'file';" | tail -n +2 | sed 's/\t/\ /g' | sed 's/NULL/0/g')
                 echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},type=full_project bytes_used=${bytes_used},inodes=${inodes},sz0=${sz0},sz1=${sz1},sz32=${sz32},sz1K=${sz1K},sz32K=${sz32K},sz1M=${sz1M},sz32M=${sz32M},sz1G=${sz1G},sz32G=${sz32G},sz1T=${sz1T}"
+
+	type_values=$(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select type,sum(count) from ACCT_STAT where projid = '"${p}"' group by type;" | tail -n +2 | sed 's/\t/=/' | xargs | sed 's/\ /,/g')
+	echo "lustre_robinhood_quota,fs=${fs},project_id=${p},project_name=${proj_name},report=inode_type ${type_values}"
 done
 
 ## GET FS-Wide File Histograms
@@ -36,5 +43,8 @@ echo "lustre_robinhood_quota,fs=${fs},histogram_type=file sz0=${sz0},sz1=${sz1},
 
 IFS=" " read -r sz0 sz1 sz32 sz1K sz32K sz1M sz32M sz1G sz32G sz1T <<< $(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select sum(sz0),sum(sz1),sum(sz32),sum(sz1K),sum(sz32K),sum(sz1M),sum(sz32M),sum(sz1G),sum(sz32G),sum(sz1T) from ACCT_STAT where type = 'dir';" | tail -n +2 | sed 's/\t/\ /g')
 echo "lustre_robinhood_quota,fs=${fs},histogram_type=dir sz0=${sz0},sz1=${sz1},sz32=${sz32},sz1K=${sz1K},sz32K=${sz32K},sz1M=${sz1M},sz32M=${sz32M},sz1G=${sz1G},sz32G=${sz32G},sz1T=${sz1T}"
+
+type_values=$(mysql -u ${robinhood_user} -p"${robinhood_password}" -D ${robinhood_db} -e "select type,sum(count) from ACCT_STAT group by type;" | tail -n +2 | sed 's/\t/=/' | xargs | sed 's/\ /,/g')
+echo "lustre_robinhood_quota,fs=${fs},report=inode_type ${type_values}"
 
 rm -rf "${tfile}"

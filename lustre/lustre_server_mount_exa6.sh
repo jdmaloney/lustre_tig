f04c4d67027d605ca20b679596b668905008a17b
malone12 <malone12@illinois.edu>
2023-09-11 22:19:42 -0500

    Added tag for FS on server mount check

--- lustre/lustre_server_mount_exa6.sh
+++ lustre/lustre_server_mount_exa6.sh
@@ -3,6 +3,8 @@
 tfile=$(mktemp /tmp/esmount.XXXXXX)
 tfile2=$(mktemp /tmp/esmount2.XXXXXX)
 
+source /etc/telegraf/lustre/lustre_config
+
 sudo /usr/bin/es_mount --status --all | grep "/dev/" | cut -d'|' -f 2,4 | sed 's/t\ m/t_m/' | sed 's/\ |\ /\ /' > "${tfile}"
 sudo /usr/bin/es_mount --status | grep "/dev/" | cut -d'|' -f 2,4 | sed 's/t\ m/t_m/' | sed 's/\ |\ /\ /' | grep -v mgs > "${tfile2}"
 
@@ -17,14 +19,14 @@ while IFS= read -r line; do
 	optimal=$(grep "${name}" "${tfile2}" | awk '{print $2}')
 	if [ -n "${optimal}" ] && [ "${name}" != "mgs" ] && [ ${mount_health} -eq 1 ]; then
 		if [ "${optimal}" == "Mounted" ]; then
-			echo "es_mount,disk_type=${disk_type},disk_name=${name} mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=1"
+			echo "es_mount,disk_type=${disk_type},disk_name=${name},fs="${fs}" mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=1"
 		else
-			echo "es_mount,disk_type=${disk_type},disk_name=${name} mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=0"
+			echo "es_mount,disk_type=${disk_type},disk_name=${name},fs="${fs}" mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=0"
 		fi
 	elif [ ${mount_health} -eq 1 ] && [ -z "${optimal}" ] && [ "${name}" != "mgs" ]; then
-		echo "es_mount,disk_type=${disk_type},disk_name=${name} mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=0"
+		echo "es_mount,disk_type=${disk_type},disk_name=${name},fs="${fs}" mount_state=\"${mount_state}\",mount_health=${mount_health},optimal_mount=0"
 	else
-		echo "es_mount,disk_type=${disk_type},disk_name=${name} mount_state=\"${mount_state}\",mount_health=${mount_health}"
+		echo "es_mount,disk_type=${disk_type},disk_name=${name},fs="${fs}" mount_state=\"${mount_state}\",mount_health=${mount_health}"
 	fi
 done < "${tfile}"
 

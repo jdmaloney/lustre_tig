c08bef0d2b9796fc3a5e6c99a17bbc160513cd70
malone12 <malone12@illinois.edu>
2024-05-11 11:54:05 -0500

    Handle multiple changelog consumers dynamically

--- lustre/lustre_changelog_check.sh
+++ lustre/lustre_changelog_check.sh
@@ -13,10 +13,13 @@ else
 	mdt=$(head -n 1 "${tfile}" | cut -d'-' -f 2 | cut -d'.' -f 1)
 	changelog_size=$(grep changelog_size "${tfile}" | cut -d'=' -f 2)
 	current_index=$(awk '$1 ~ "^current_index.*" {print $2}' "${tfile}")
-	consumer_index=$(awk '$1 == "cl1" {print $2}' "${tfile}")
-	backlog=$((current_index-consumer_index))
-
-	echo "lustre_backlog,mdt=${mdt},fs=${fs} backlog=${backlog},current_index=${current_index},consumer_index=${consumer_index},changelog_size=${changelog_size}"
+	indexes=($(grep -A20 ID "${tfile}" | awk '{print $1}' | grep -v ID | xargs))
+	for i in ${indexes[@]}
+	do
+		consumer_index=$(awk -v i=$i '$1 == i {print $2}' "${tfile}")
+		backlog=$((current_index-consumer_index))
+		echo "lustre_backlog,mdt=${mdt},fs=${fs},index_id=${i} backlog=${backlog},current_index=${current_index},consumer_index=${consumer_index},changelog_size=${changelog_size}"
+	done
 
 	rm -rf ${tfile}
 fi

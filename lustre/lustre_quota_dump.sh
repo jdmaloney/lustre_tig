65c92a22ee110f8eddf9242b1adf23c7668488a1
malone12 <malone12@illinois.edu>
2025-07-30 13:29:36 -0500

    Re-worked Lustre quota parse for better performance

--- lustre/lustre_quota_dump.sh
+++ lustre/lustre_quota_dump.sh
@@ -2,35 +2,23 @@
 
 source /etc/telegraf/lustre/lustre_config
 
-targets=($(ls /proc/fs/lustre/osd-ldiskfs/ | grep -v -i mgs))
+targets=($(ls /proc/fs/lustre/osd-ldiskfs/ | grep -i mdt))
 
 for t in ${targets[@]}
 do
-	pool=$(awk -v target=${t} '$2 == target {print $1}' ${admin_dir}/pool_map)
-	target_type=$(echo "${t}" | cut -d'-' -f 2 | cut -c 1-3)
-	if [ "${target_type}" == "OST" ]; then
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_user | tail -n +2 | paste - - -d" " | awk '{print $3" "$9}' | sed 's/,//g' > ${admin_dir}/user_${t}_${pool}.txt
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_group | tail -n +2 | paste - - -d" " | awk '{print $3" "$9}' | sed 's/,//g' > ${admin_dir}/group_${t}_${pool}.txt
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_project | tail -n +2 | paste - - -d" " | awk '{print $3" "$9}' | sed 's/,//g' > ${admin_dir}/project_${t}_${pool}.txt
-		OST_index=$(echo "${t}" | cut -d'-' -f 2 | cut -c 4-)
-                index_value=$(fold -w1 <<< ${OST_index} | paste -sd+ - | bc)
-                if [ ${index_value} -eq 0 ]; then
-                        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_user | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/blimit_user_${t}_${pool}.txt
-                        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_group | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/blimit_group_${t}_${pool}.txt
-                        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_project | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/blimit_project_${t}_${pool}.txt
-                fi
-	elif [ "${target_type}" == "MDT" ]; then
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_user | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/user_${t}_${pool}.txt
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_group | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/group_${t}_${pool}.txt
-		cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_project | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/project_${t}_${pool}.txt
-		MDT_index=$(echo "${t}" | cut -d'-' -f 2 | cut -c 4-)
-		index_value=$(fold -w1 <<< ${MDT_index} | paste -sd+ - | bc)
-		if [ ${index_value} -eq 0 ]; then
-			cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_user | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/flimit_user_${t}.txt
-	                cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_group | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/flimit_group_${t}.txt
-        	        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/limit_project | tail -n +2 | paste - - -d" " | awk '{print $3" "$7" "$9}' | sed 's/,//g' > ${admin_dir}/flimit_project_${t}.txt
-		fi
-	else
-		echo "Error in type"
-	fi
+        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_user | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/user_${t}_MDT.txt
+        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_group | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/group_${t}_MDT.txt
+        cat /proc/fs/lustre/osd-ldiskfs/${t}/quota_slave/acct_project | tail -n +2 | paste - - -d" " | awk '{print $3" "$9" "$7}' | sed 's/,//g' > ${admin_dir}/project_${t}_MDT.txt
+done
+
+target_checks=($(lctl get_param osd-ldiskfs.*.quota_slave.enabled | xargs))
+for t in ${target_checks[@]}
+do
+        target=$(echo ${t} | cut -d'.' -f 2)
+        state=$(echo ${t} | cut -d'=' -f 2)
+        if [ "${state}" == "${expected_q_state}" ]; then
+                echo "lustre_q_enforce_check,fs=${fs},target=${target} enforce_error=0,state=\"${state}\""
+        else
+                echo "lustre_q_enforce_check,fs=${fs},target=${target} enforce_error=1,state=\"${state}\""
+        fi
 done
--- lustre/lustre_quota_parse.sh
+++ lustre/lustre_quota_parse.sh
@@ -2,254 +2,82 @@
 
 source /etc/telegraf/lustre/lustre_config
 
-track_file=$(mktemp /tmp/qtrack.XXXXXX)
-pools=($(awk '{print $1}' ${admin_dir}/pool_map | sort -u))
+tfile=$(mktemp /tmp/lustrequota.XXXXXXXX)
 
-parse_user_quotas () {
-	tfile1=$(mktemp /tmp/qparse1.XXXXXX)
-	tfile2=$(mktemp /tmp/qparse2.XXXXXX)
-	tfile3=$(mktemp /tmp/qparse3.XXXXXX)
-	uids=($(cat ${admin_dir}/user*.txt | awk '{print $1}' | sort -u))
+pools=($(/usr/sbin/lctl pool_list $filesystem | tail -n +2 | grep -v old | xargs))
+mdts=($(ls ${admin_dir}/project_* | cut -d'/' -f 3 | cut -d'_' -f 2))
 
-	for o in ${pools[@]}
-	do
-		if [ ${o} == "all" ]; then
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/user*.txt > ${tfile1}.${o}
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_user*.txt > ${tfile2}.${o}
-		else
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/user*_${o}.txt ${admin_dir}/user*_all.txt > ${tfile1}.${o}
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_user*_${o}.txt > ${tfile2}.${o}
-		fi
-	done
-	awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/flimit_user*.txt > ${tfile3}
-	blimit_count=$(cat ${tfile2}.* | wc -l | awk '{print $1}')
-	flimit_count=$(wc -l ${tfile3} | awk '{print $1}')
+for i in ${pools[@]}
+do
+        pool_name=$(echo $i | cut -d'.' -f 2)
+        /usr/bin/lfs quota --pool ${i} -u -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+        while read -r p; do
+                IFS="," read -r user kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+                echo "lustre_quota,fs=${fs},pool=${pool_name},user=${user} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+        done<"${tfile}"
 
-	for u in ${uids[@]}
-	do
-		user_name=$(awk -v u="${u}" -F: '$3 == u {print $1}' ${admin_dir}/getent_passwd)
-		if [ -z "${user_name}" ]; then
-			user_name="NotFound"
-		fi
-		if [ ${flimit_count} -gt 2 ]; then
-	                read -r file_limit file_quota <<< $(awk -v u=${u} '$1 == u {print $2" "$3}' ${tfile3})
-	                if [ -z "${file_limit}" ]; then
-	       	                 file_limit=0
-	                fi
-	                if [ -z "${file_quota}" ]; then
-	                        file_quota=0
-			fi
-		else
-	                file_limit=0
-	                file_quota=0
-	        fi
-		for o in ${pools[@]}
-		do
-			read -r kb_used inodes <<< $(awk -v u=${u} '$1 == u {print $2" "$3}' ${tfile1}.${o})
-			if [ -z "${kb_used}" ]; then
-				kb_used=0
-			fi
-			if [ -z "${inodes}" ]; then
-				inodes=0
-			fi
-			if [ ${blimit_count} -gt 2 ]; then
-		                read -r kb_limit kb_quota <<< $(awk -v u=${u} '$1 == u {print $2" "$3}' ${tfile2}.${o})
-		                if [ -z "${kb_limit}" ]; then
-		                        kb_limit=0
-		                fi
-		                if [ -z "${kb_quota}" ]; then
-		                        kb_quota=0
-		                fi
-		        else
-		                kb_limit=0
-		                kb_quota=0
-		        fi
-			echo "lustre_quota,fs=${fs},pool=${o},user_id=${u},user=${user_name} kb_used=${kb_used},kb_quota=${kb_quota},kb_limit=${kb_limit},files=${inodes},file_quota=${file_quota},file_limit=${file_limit}"
-		done
-		mdts=($(ls ${admin_dir}/user*MDT*.txt | rev | cut -d'_' -f 2 | rev))
-		for m in ${mdts[@]}
-		do
-			read -r kb_used inodes <<< $(awk -v u=${u} '$1 == u {print $2" "$3}' ${admin_dir}/user_${m}_all.txt)
-        	        if [ -z "${kb_used}" ]; then
-        	                kb_used=0
-        	        fi
-        	        if [ -z "${inodes}" ]; then
-        	                inodes=0
-        	        fi
-			echo "lustre_quota,fs=${fs},pool=mdt,user_id=${u},user=${user_name},mdt=${m} kb_used=${kb_used},files=${inodes}"
-		done
-	done
-        rm -f /tmp/qparse1*
-        rm -f /tmp/qparse2*
-        rm -f /tmp/qparse3*
-	echo "done" >> ${track_file}
-}
+        /usr/bin/lfs quota --pool ${i} -g -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+        while read -r p; do
+                IFS="," read -r group kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+                echo "lustre_quota,fs=${fs},pool=${pool_name},group=${group} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+        done<"${tfile}"
 
-
-parse_group_quotas () {
-	tfile4=$(mktemp /tmp/qparse4.XXXXXX)
-        tfile5=$(mktemp /tmp/qparse5.XXXXXX)
-        tfile6=$(mktemp /tmp/qparse6.XXXXXX)
-	gids=($(cat ${admin_dir}/group*.txt | awk '{print $1}' | sort -u))
-
-	for o in ${pools[@]}
-	do
-		if [ ${o} == "all" ]; then
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/group*.txt > ${tfile4}.${o}
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_group*.txt > ${tfile5}.${o}
-		else
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/group*_${o}.txt ${admin_dir}/group*_all.txt > ${tfile4}.${o}
-	                awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_group*_${o}.txt > ${tfile5}.${o}
-		fi
-	done
-	awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/flimit_group*.txt > ${tfile6}
-	blimit_count=$(cat ${tfile5}.* | wc -l | awk '{print $1}')
-	flimit_count=$(wc -l ${tfile6} | awk '{print $1}')
-
-	for g in ${gids[@]}
-	do
-		group_name=$(awk -v g="${g}" -F: '$3 == g {print $1}' ${admin_dir}/getent_group)
-		if [ -z "${group_name}" ]; then
-	                group_name="NotFound"
-	        fi
-		if [ ${flimit_count} -gt 2 ]; then
-			read -r file_limit file_quota <<< $(awk -v g=${g} '$1 == g {print $2" "$3}' ${tfile6})
-			if [ -z "${file_limit}" ]; then
-		                file_limit=0
-		        fi
-		        if [ -z "${file_quota}" ]; then
-		                file_quota=0
-		        fi
-		else
-			file_limit=0
-			file_quota=0
-		fi
-		for o in ${pools[@]}
-		do
-		        read -r kb_used inodes <<< $(awk -v g=${g} '$1 == g {print $2" "$3}' ${tfile4}.${o})
-	                if [ -z "${kb_used}" ]; then
-	                        kb_used=0
-	                fi
-	                if [ -z "${inodes}" ]; then
-	                        inodes=0
-	                fi
-			if [ ${blimit_count} -gt 2 ]; then
-				read -r kb_limit kb_quota <<< $(awk -v g=${g} '$1 == g {print $2" "$3}' ${tfile5}.${o})
-				if [ -z "${kb_limit}" ]; then
-			                kb_limit=0
-			        fi
-			        if [ -z "${kb_quota}" ]; then
-			                kb_quota=0
-			        fi
-			else
-				kb_limit=0
-				kb_quota=0
-			fi
-		        echo "lustre_quota,fs=${fs},pool=${o},group_id=${g},group=${group_name} kb_used=${kb_used},kb_quota=${kb_quota},kb_limit=${kb_limit},files=${inodes},file_quota=${file_quota},file_limit=${file_limit}"
-		done
-		mdts=($(ls ${admin_dir}/group*MDT*.txt | rev | cut -d'_' -f 2 | rev))
-	        for m in ${mdts[@]}
-	        do
-	                read -r kb_used inodes <<< $(awk -v g=${g} '$1 == g {print $2" "$3}' ${admin_dir}/group_${m}_all.txt)
-	  		if [ -z "${kb_used}" ]; then
-	                        kb_used=0
-	                fi
-	                if [ -z "${inodes}" ]; then
-	                        inodes=0
-	                fi
-			echo "lustre_quota,fs=${fs},pool=mdt,group_id=${g},group=${group_name},mdt=${m} kb_used=${kb_used},files=${inodes}"
-	        done
-	done
-	rm -f /tmp/qparse4*
-        rm -f /tmp/qparse5*
-        rm -f /tmp/qparse6*
-	echo "done" >> ${track_file}
-}
-
-parse_project_quotas () {
-	tfile7=$(mktemp /tmp/qparse7.XXXXXX)
-        tfile8=$(mktemp /tmp/qparse8.XXXXXX)
-        tfile9=$(mktemp /tmp/qparse9.XXXXXX)
-	pids=($(cat ${admin_dir}/project*.txt | awk '{print $1}' | sort -u))
-
-	for o in ${pools[@]}
-	do
-		if [ ${o} == "all" ]; then
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/project*.txt > ${tfile7}.${o}
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_project*.txt > ${tfile8}.${o}
-		else
-			awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/project*_${o}.txt ${admin_dir}/project*_all.txt > ${tfile7}.${o}
-	                awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/blimit_project*_${o}.txt > ${tfile8}.${o}
-		fi
-	done
-	awk '{a[$1]+=$2;b[$1]+=$3}END{for(i in a)print i, a[i], b[i]|"sort"}' ${admin_dir}/flimit_project*.txt > ${tfile9}
-	blimit_count=$(cat ${tfile8}.* | wc -l | awk '{print $1}')
-	flimit_count=$(wc -l ${tfile9} | awk '{print $1}')
-
-	for p in ${pids[@]}
-	do
-		project_name=$(awk -v p="${p}" -F: '$3 == p {print $1}' ${admin_dir}/getent_group)
-		if [ -z "${project_name}" ]; then
-	                project_name="NotFound"
-	        fi
-		if [ ${flimit_count} -gt 2 ]; then
-			read -r file_limit file_quota <<< $(awk -v p=${p} '$1 == p {print $2" "$3}' ${tfile9})
-			if [ -z "${file_limit}" ]; then
-				file_limit=0
-			fi
-			if [ -z "${file_quota}" ]; then
-				file_quota=0
-			fi
-		else
-			file_limit=0
-			file_quota=0
-		fi
-		for o in ${pools[@]}; do
-		        read -r kb_used inodes <<< $(awk -v p=${p} '$1 == p {print $2" "$3}' ${tfile7}.${o})
-	                if [ -z "${kb_used}" ]; then
-	                        kb_used=0
-	                fi
-	                if [ -z "${inodes}" ]; then
-	                        inodes=0
-	                fi
-			if [ ${blimit_count} -gt 2 ]; then
-		                read -r kb_limit kb_quota <<< $(awk -v p=${p} '$1 == p {print $2" "$3}' ${tfile8}.${o})
-		                if [ -z "${kb_limit}" ]; then
-		                        kb_limit=0
-		                fi
-		                if [ -z "${kb_quota}" ]; then
-		                        kb_quota=0
-		                fi
-		        else
-		                kb_limit=0
-		                kb_quota=0
-			        fi
-			echo "lustre_quota,fs=${fs},pool=${o},project_id=${p},project_name=${project_name} kb_used=${kb_used},kb_quota=${kb_quota},kb_limit=${kb_limit},files=${inodes},file_quota=${file_quota},file_limit=${file_limit}"
-		done
-		mdts=($(ls ${admin_dir}/project*MDT*.txt | rev | cut -d'_' -f 2 | rev))
-	        for m in ${mdts[@]}
-	        do
-	                read -r kb_used inodes <<< $(awk -v p=${p} '$1 == p {print $2" "$3}' ${admin_dir}/project_${m}_all.txt)
-			if [ -z "${kb_used}" ]; then
-				kb_used=0
-			fi
-			if [ -z "${inodes}" ]; then
-				inodes=0
-			fi
-	                echo "lustre_quota,fs=${fs},pool=mdt,project_id=${p},project_name=${project_name},mdt=${m} kb_used=${kb_used},files=${inodes}"
-	        done
-	done
-	rm -f /tmp/qparse7*
-	rm -f /tmp/qparse8*
-	rm -f /tmp/qparse9*
-	echo "done" >> ${track_file}
-}
-
-parse_user_quotas &
-parse_group_quotas &
-parse_project_quotas &
-
-while [ $(wc -l ${track_file} | awk '{print $1}') -lt 3 ]; do
-	sleep 0.5
+        /usr/bin/lfs quota --pool ${i} -p -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+        while read -r p; do
+                IFS="," read -r prjid kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+                project=$(awk -v prjid=$prjid -F ':' '$3 == prjid {print $1}' ${quota_group})
+                project=${project:=none}
+                echo "lustre_quota,fs=${fs},pool=${pool_name},project=${project} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+        done<"${tfile}"
 done
-rm -f /tmp/qtrack.*
+
+pool_name=all
+
+/usr/bin/lfs quota -u -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+while read -r p; do
+        IFS="," read -r user kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+        echo "lustre_quota,fs=${fs},pool=${pool_name},user=${user} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+done<"${tfile}"
+
+/usr/bin/lfs quota -g -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+while read -r p; do
+        IFS="," read -r group kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+        echo "lustre_quota,fs=${fs},pool=${pool_name},group=${group} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+done<"${tfile}"
+
+/usr/bin/lfs quota -p -a ${fs} | awk '{print $2","$3","$4","$5","$7","$8","$9}' | tail -n +3 > "${tfile}"
+while read -r p; do
+        IFS="," read -r prjid kb_used kb_soft kb_hard inode_used inode_soft inode_hard <<< $(echo ${p} | sed 's/\[//g' | sed 's/\]//g' |  sed 's/*//g')
+        project=$(awk -v prjid=$prjid -F ':' '$3 == prjid {print $1}' ${quota_group})
+        project=${project:=none}
+        echo "lustre_quota,fs=${fs},pool=${pool_name},project=${project} kb_used=${kb_used},kb_soft=${kb_soft},kb_hard=${kb_hard},inode_used=${inode_used},inode_soft=${inode_soft},inode_hard=${inode_hard}"
+done<"${tfile}"
+
+rm -f "${tfile}"
+
+parse_mdt_file () {
+        if [ $1 == "user" ]; then
+                lookup_file=${quota_passwd}
+        else
+                lookup_file=${quota_group}
+        fi
+        while read -r p; do
+                IFS=" " read -r id kb_used inode_used <<< $(echo ${p})
+                name=$(awk -v id=$id -F ':' '$3 == id {print $1}' ${lookup_file} | head -n 1)
+                name=${name:=none}
+                echo "lustre_quota,fs=${fs},pool=MDT,${t}=${name},target=${2} kb_used=${kb_used},inode_used=${inode_used}"
+        done<"${admin_dir}/${t}_${2}_MDT.txt"
+}
+
+for m in ${mdts[@]}
+do
+        for t in user group project; do
+                parse_mdt_file ${t} ${m} &
+        done
+done
+
+threads=$(ps -ef | grep "lustre_quota_parse" | grep -v grep |  wc -l)
+while [ ${threads} -gt 3 ]; do
+        sleep 0.2
+        threads=$(ps -ef | grep "lustre_quota_parse" | grep -v grep |  wc -l)
+done
